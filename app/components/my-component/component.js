e09e6d057002b75c7458eca03d2458831d4cf7f0
Lux <lux@lux.name>
2019-07-05 10:48:33 +0200

    more logs for reproduction

--- app/components/my-component/component.js
+++ app/components/my-component/component.js
@@ -6,10 +6,12 @@ const timeout = ms => new Promise(resolve => setTimeout(resolve, ms));
 export default Component.extend({
   actions: {
     async go() {
-      console.log(this.get('foo.bar.value'));
+      console.log(`this.get('foo.bar.value')`, this.get('foo.bar.value'));
+      console.log(`this.get('foo').get('bar.value')`, this.get('foo').get('bar.value'));
       this.transitionToIndex();
       await timeout(1);
-      console.log(this.get('foo.bar.value'));
+      console.log(`this.get('foo.bar.value')`, this.get('foo.bar.value'));
+      console.log(`this.get('foo').get('bar.value')`, this.get('foo').get('bar.value'));
     }
   }
 });
